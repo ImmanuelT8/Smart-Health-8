f83d46b9bda66426f1c7940c578ef1dab9f88e20
ImmanuelT8 <64651354+ImmanuelT8@users.noreply.github.com>
2023-04-19 21:54:05 +0200

    Create SPO2_Filter.h

---
+++ SPO2/temp/SPO2_Filter.h
@@ -0,0 +1,51 @@
+#ifndef SPO2_Filter_H
+#define SPO2_Filter_H
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <math.h>
+
+
+#define MEAN_FILTER_SIZE        15  
+
+
+struct meanDiffFilter_t
+{
+  float values[MEAN_FILTER_SIZE];
+  byte index;
+  float sum;
+  byte count;
+};
+
+
+
+class MAX30100 
+{
+  public:
+  float meanDiff();
+
+  float filterValues; 
+  void setvalues(uint16_t filtervalues);
+
+
+private:
+  meanDiffFilter_t meanDiffIR;
+
+
+};
+
+
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
