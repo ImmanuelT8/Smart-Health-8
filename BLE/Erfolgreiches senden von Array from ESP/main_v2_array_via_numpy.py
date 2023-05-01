99859484316bc57d36e315417fe9dfb24a68d38c
ImmanuelT8 <64651354+ImmanuelT8@users.noreply.github.com>
2023-05-01 21:16:13 +0200

    Create main_v2_array_via_numpy.py

---
+++ BLE/Erfolgreiches senden von Array from ESP/main_v2_array_via_numpy.py
@@ -0,0 +1,41 @@
+import asyncio
+from bleak import BleakClient
+import numpy
+
+address = "CC:50:E3:9C:15:02"
+MODEL_NBR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+
+
+# async def read_spo2(address):
+#    SPO2_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+#    async with BleakClient(address) as client:
+#        while True:  # run in an infinite loop
+#           spo2_data = await client.read_gatt_char(SPO2_UUID)
+#           print("SPO2: {0}".format(spo2_value))
+#           await asyncio.sleep(1)  # wait for 1 second
+
+
+
+async def read_spo2(address):
+    SPO2_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
+    async with BleakClient(address) as client:
+        await client.start_notify(SPO2_UUID, notification_handler)
+        # Wait for notifications
+        while True:
+            await asyncio.sleep(1)
+
+async def notification_handler(sender, data):
+
+    spo2_value = numpy.frombuffer(data, dtype=numpy.uint32)
+
+    print("Rotwerte:")
+    print(spo2_value)
+
+
+
+
+async def main(address):
+    await read_spo2(address)
+
+asyncio.run(main(address))
+
