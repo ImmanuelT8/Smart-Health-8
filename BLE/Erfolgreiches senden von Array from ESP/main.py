09da4fd029f20ecb8cff68987103d6ac04e910d1
ImmanuelT8 <64651354+ImmanuelT8@users.noreply.github.com>
2023-05-01 20:33:22 +0200

    Update main.py

--- BLE/Erfolgreiches senden von Array from ESP/main.py
+++ BLE/Erfolgreiches senden von Array from ESP/main.py
@@ -4,10 +4,6 @@ from bleak import BleakClient
 address = "CC:50:E3:9C:15:02"
 MODEL_NBR_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-async def main(address):
-    async with BleakClient(address) as client:
-        model_number = await client.read_gatt_char(MODEL_NBR_UUID)
-        print("Model Number: {0}".format("".join(map(chr, model_number))))
 
 # async def read_spo2(address):
 #    SPO2_UUID = "beb5483e-36e1-4688-b7f5-ea07361b26a8"
@@ -29,23 +25,13 @@ async def read_spo2(address):
 
 async def notification_handler(sender, data):
     spo2_value = int.from_bytes(data, byteorder='little')
-    print("SPO2: {0}".format(spo2_value))
+    print("Red: {0}".format(spo2_value))
+
+
 
 
 async def main(address):
     await read_spo2(address)
 
 asyncio.run(main(address))
-
-
-
-
-
-
-
-
-
-
-
-
 
